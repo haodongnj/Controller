1408f761659ce8d4cbab46985173521d0309556d
donghao2nanjing <dongyinhao@foxmail.com>
2018-03-15 21:38:33 +0800

    Add current control parameter

--- control_para.h
+++ control_para.h
@@ -8,9 +8,10 @@
 #ifndef CONTROLLER_CONTROL_PARA_H_
 #define CONTROLLER_CONTROL_PARA_H_
 
-#define CTRL_KP 0.006f
-#define CTRL_KI 10.0f
+#define V_CTRL_KP 0.006f
+#define V_CTRL_KI 10.0f
 
-
+#define C_CTRL_KP 0.0f
+#define C_CTRL_KI 0.0f
 
 #endif /* CONTROLLER_CONTROL_PARA_H_ */
