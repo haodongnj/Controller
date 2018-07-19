41a9cd0a1f2aacc8f2f0464ef028f8387525224b
donghao2nanjing <dongyinhao@foxmail.com>
2018-07-19 22:04:48 +0800

    Add interpolation of Repetitive Controller

---
+++ IRC/irc.h
@@ -0,0 +1,39 @@
+#ifndef IRC_H
+#define IRC_H
+
+#include "filter.h"
+
+#define NUMBER_SAMPLES_PERIOD 200U
+#define SIMULINK_DEBUG 1 
+#define SLICE_ANGLE 0.0314159265359f
+#define DOUBLE_PI  6.28318530718f
+
+typedef struct irc_structure{
+    double rc_input_buffer[NUMBER_SAMPLES_PERIOD];
+    double rc_output_buffer[NUMBER_SAMPLES_PERIOD] ;
+    double rc_filtered_buffer[NUMBER_SAMPLES_PERIOD] ;
+    int cur_index ;
+    int steps_lead ;
+    double q_center ;
+    double q_lr ;
+    double k_rc ;
+    double last_phase ;
+    double last_left_index ;
+    double last_error ;
+    filter_s fs ;
+}irc_struct;
+
+#if SIMULINK_DEBUG 
+
+void irc_init_simulink();
+double irc_update_simulink(double error, double phase);
+
+#endif
+
+double left_slice(double cur_phase);
+int left_index(double cur_phase);
+int steps_to_interp(int cur_index, int last_index);
+double phase_diff(double phase, double phase_to_diff);
+int index_move(int cur_index, int steps);
+
+#endif
