46be73d4f507ae9b9dbc22956bfb34c458348eb8
donghao2nanjing <dongyinhao@foxmail.com>
2018-08-21 23:07:27 +0800

    backup

--- dvrc/dvrc.c
+++ dvrc/dvrc.c
@@ -2,9 +2,9 @@
 
 #if SIMULINK_DEBUG 
 dvrc_struct dvrc_s ;
-dvrc_struct * p_dvrc_s ;
+dvrc_struct * p_dvrc_s = & dvrc_s;
 filter_s f_s ;
-filter_s * p_f_s ;
+filter_s * p_f_s = &f_s ;
 
 double num_filter[3] = {0.0f, 0.00726436941467171, 0.00668055316076471};
 double den_filter[3] = {1.0f, -1.76382275659635, 0.777767679171789};
@@ -16,19 +16,18 @@ void init_dvrc_simulink(){
     double phase_angle ;
     for(i = 0 ; i < MAX_STEPS_FOR_BUFFER; i ++){
         phase_angle = i * SLICE_ANGLE ;
-        phase_angle = phase_angle > DOUBLE_PI_DVRC ? 
-                        (phase_angle - DOUBLE_PI_DVRC) : (phase_angle) ;
+        phase_angle = (phase_angle > DOUBLE_PI_DVRC) ? (phase_angle - DOUBLE_PI_DVRC) : (phase_angle) ;
         p_dvrc_s->phase_buffer[i] =  phase_angle ;
         p_dvrc_s->q_output_buffer[i] = 0.0 ;
         p_dvrc_s->delay_prev_buffer[i] = 0.0 ;
     }
-    p_dvrc_s->lead_steps = 9 ;
+    p_dvrc_s->lead_steps = 0 ;
 
     // Setup the coeff of  filter
     filter_init(p_f_s, num_filter, den_filter, order_filter) ;
     p_dvrc_s->pfs = p_f_s ;
 
-    p_dvrc_s->q_main = 0.9 ;
+    p_dvrc_s->q_main = 0.95 ;
     p_dvrc_s->q_lr = (1 - p_dvrc_s->q_main) * 0.5 ;
 }
 
@@ -46,9 +45,9 @@ double calc_dvrc_simulink(double phase, double error, int flag_start_calc){
         // update buffer of q filtered buffer:
         index_delay_selected = index_move(p_dvrc_s->delay_prev_index, -steps_delay) ;
         ret_dvrc = // Used to store the value just to avoid a extra variable definition.
-            p_dvrc_s->q_output_buffer[index_delay_selected] * p_dvrc_s->q_main + 
-            p_dvrc_s->q_output_buffer[index_move(index_delay_selected, 1)] * p_dvrc_s->q_lr + 
-            p_dvrc_s->q_output_buffer[index_move(index_delay_selected, -1)] * p_dvrc_s->q_lr ;
+            p_dvrc_s->delay_prev_buffer[index_delay_selected] * p_dvrc_s->q_main + 
+            p_dvrc_s->delay_prev_buffer[index_move(index_delay_selected, 1)] * p_dvrc_s->q_lr + 
+            p_dvrc_s->delay_prev_buffer[index_move(index_delay_selected, -1)] * p_dvrc_s->q_lr ;
 
         p_dvrc_s->q_output_buffer[p_dvrc_s->q_output_index] = ret_dvrc ;
 
@@ -83,6 +82,7 @@ int index_move(int cur_index, int steps_to_move){
     else if( ret_index < 0 ){
         ret_index += MAX_STEPS_FOR_BUFFER ;
     }
+    return ret_index ;
 }
 
 int find_varying_delay(dvrc_struct * ps, double cur_phase){
