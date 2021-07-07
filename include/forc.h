1833cfd717c9e26c760566e926bb67610fa50d61
hao.dong <hao.dong.nj@outlook.com>
2021-07-07 22:31:18 +0800

    tmp commit

--- include/forc.h
+++ include/forc.h
@@ -1,48 +1,41 @@
-#ifndef WDVRC_H
-#define WDVRC_H
+#ifndef FORC_H
+#define FORC_H
 
-#include "filter.h"
-#include "math.h"
+#include <filter.h>
 
-#define MAX_STEPS_FOR_BUFFER 410
-#define STEPS_AT_BASE_FREQ 400
-#define SAMPLE_FREQUENCY 20000
+#ifdef __cplusplus
+extern "C"{
+#endif
 
-#define DEBUG 0
-
-typedef struct fir_rc_structure{
-    float q_output_buffer[MAX_STEPS_FOR_BUFFER] ;
-
-    float delay_prev_buffer[MAX_STEPS_FOR_BUFFER]  ;
-
-    float q_input_buffer[MAX_STEPS_FOR_BUFFER] ;
-
-    int q_input_index ;
-    int q_output_index ;
-    int delay_prev_index ;
-
-    int lead_steps ;
-    filter_s * pfs ;
+typedef struct FORC{
     float q_main ;
     float q_lr ;
 
-    float k_rc ;
-    float w_dr ; // delay 1 more step ; e.g. 202
-    float w_dl ; // delay 1 less step ; e.g. 201
-}fir_rc_struct ;
+    float *output_buffer ;
+    float *delay_prev_buffer;
+    float *input_buffer;
 
-int index_move(int cur_index, int steps_to_move) ;
+    int idx_q_input;
+    int idx_q_output;
+    int idx_prev;
 
-#if DEBUG
+    int max_steps_each_period;
+    int f_control;
 
-void init_fir_rc_debug() ;
-float calc_fir_rc_debug(float freq_grid, float error);
+    int lead_steps ;
+    Filter_t *p_compensator;
+    float k ;
 
-#else
+}FORC_t;
 
-void init_fir_rc(fir_rc_struct * p_dvrc_s, filter_s * p_f_s, float q_main,  float k_rc, int steps_lead);
-float calc_fir_rc(fir_rc_struct * p_dvrc_s, float freq_grid, float error);
+int index_forward(FORC_t s, int cur_index, int steps_forward);
 
+void init_forc(FORC_t *s, float q_main, int lead_steps, float k, Filter* p_compensator);
+
+float calc_forc(FORC_t *s, float error, float f);
+
+#ifdef __cplusplus
+};
 #endif
 
 #endif
--- src/pwrc.c
+++ src/pwrc.c
@@ -1,106 +1,11 @@
-#include "../include/pwrc.h"
+#include <pwrc.h>
 
-#if SIMULINK_DEBUG 
-dvrc_struct dvrc_s ;
-dvrc_struct * p_dvrc_s = & dvrc_s;
-filter_s f_s ;
-filter_s * p_f_s = &f_s ;
-
-double num_filter[3] = {0.0f, 0.00726436941467171, 0.00668055316076471};
-double den_filter[3] = {1.0f, -1.76382275659635, 0.777767679171789};
-int order_filter = 2 ;
-
-
-void init_wdvrc_simulink(){
-    int i ; 
-    double phase_angle ;
-    for(i = 0 ; i < MAX_STEPS_FOR_BUFFER; i ++){
-        phase_angle = i * SLICE_ANGLE ;
-        phase_angle = (phase_angle > DOUBLE_PI_DVRC) ? (phase_angle - DOUBLE_PI_DVRC) : (phase_angle) ;
-        p_dvrc_s->phase_buffer[i] =  phase_angle ;
-        p_dvrc_s->q_output_buffer[i] = 0.0 ;
-        p_dvrc_s->delay_prev_buffer[i] = 0.0 ;
-    }
-    p_dvrc_s->lead_steps = 1;
-
-    // Setup the coeff of  filter
-    filter_init(p_f_s, num_filter, den_filter, order_filter) ;
-    p_dvrc_s->pfs = p_f_s ;
-    p_dvrc_s->krc = 1.0 ;
-
-    p_dvrc_s->q_main = 0.95 ;
-    p_dvrc_s->q_lr = (1 - p_dvrc_s->q_main) * 0.5 ;
-    p_dvrc_s->weight_dm = 0.5 ;
-    p_dvrc_s->weight_dl = 0.5 ;
-    p_dvrc_s->error_flag = 0 ;
-
-    p_dvrc_s->q_output_index = 0 ;
-    p_dvrc_s->delay_prev_index = 0 ;
-    p_dvrc_s->phase_index = 0 ;
-}
-
-double calc_wdvrc_simulink(double phase, double error ,int flag_start_calc){
-    int steps_delay = find_varying_delay(p_dvrc_s, phase) ;
-    int index_delay_selected ;
-    double ret_dvrc = 0.0, w_dl, w_dm ;
-
-    // When in closedloop, RC results
-    if( flag_start_calc){
-        // update buffer before the delay 
-        p_dvrc_s->delay_prev_buffer[p_dvrc_s->delay_prev_index] = error + 
-            p_dvrc_s->q_output_buffer[index_move(p_dvrc_s->q_output_index, - p_dvrc_s->lead_steps)] ;
-        
-        // update buffer of q filtered buffer:
-        index_delay_selected = index_move(p_dvrc_s->delay_prev_index, -steps_delay + p_dvrc_s->lead_steps) ;
-
-        w_dl = p_dvrc_s->delay_prev_buffer[index_delay_selected] * p_dvrc_s->q_main + 
-            p_dvrc_s->delay_prev_buffer[index_move(index_delay_selected, 1)] * p_dvrc_s->q_lr + 
-            p_dvrc_s->delay_prev_buffer[index_move(index_delay_selected, -1)] * p_dvrc_s->q_lr ;
-
-        w_dl = w_dl * p_dvrc_s->weight_dl ;
-
-        index_delay_selected = index_move(index_delay_selected, -1) ;
-
-        w_dm = p_dvrc_s->delay_prev_buffer[index_delay_selected] * p_dvrc_s->q_main + 
-            p_dvrc_s->delay_prev_buffer[index_move(index_delay_selected, 1)] * p_dvrc_s->q_lr + 
-            p_dvrc_s->delay_prev_buffer[index_move(index_delay_selected, -1)] * p_dvrc_s->q_lr ;
-
-        w_dm = w_dm * p_dvrc_s->weight_dm ;
-        
-        ret_dvrc = w_dl + w_dm ;
-
-        p_dvrc_s->q_output_buffer[p_dvrc_s->q_output_index] = ret_dvrc ;
-
-        ret_dvrc = filter_calc(p_dvrc_s->pfs, ret_dvrc) ; // Lowpass filter 
-
-        // update index:
-        p_dvrc_s->q_output_index = index_move(p_dvrc_s->q_output_index, 1);
-        p_dvrc_s->delay_prev_index = index_move(p_dvrc_s->delay_prev_index, 1);
-
-    }
-
-    p_dvrc_s->phase_buffer[ p_dvrc_s-> phase_index ] = phase ;
-    // Update phase index 
-    p_dvrc_s->phase_index = index_move(p_dvrc_s->phase_index, 1) ;
-
-    // When in openloop, only phase_buffer and phase_index is updated.
-    if(!flag_start_calc){
-        return steps_delay ;
-    }else{
-        return ret_dvrc * p_dvrc_s->krc ;
-    }
-}
-
-#else
-#include "../../../../include/DSP2833x_Device.h"
-#include "../../../../include/DSP2833x_Examples.h"
-
-void init_wdvrc(dvrc_struct * p_dvrc_s, filter_s * p_f_s, float q_main, float k_rc, int steps_lead){
+void init_pwrc(PWRC_t *s, filter_s * p_f_s, float q_main, float k_rc, int steps_lead){
     int i ; 
     float phase_angle ;
     for(i = 0 ; i < MAX_STEPS_FOR_BUFFER; i ++){
         phase_angle = i * SLICE_ANGLE ;
-        phase_angle = (phase_angle > DOUBLE_PI_DVRC) ? (phase_angle - DOUBLE_PI_DVRC) : (phase_angle) ;
+        phase_angle = (phase_angle > DOUBLE_PI_DVRC) ? (phase_angle - M_PI * 2) : (phase_angle) ;
         p_dvrc_s->phase_buffer[i] =  phase_angle ;
         p_dvrc_s->q_output_buffer[i] = 0.0 ;
         p_dvrc_s->delay_prev_buffer[i] = 0.0 ;
@@ -171,8 +76,6 @@ float calc_wdvrc(dvrc_struct * p_dvrc_s, float phase, float error, int flag_star
         return ret_dvrc * p_dvrc_s->krc ;
     }
 }
-
-#endif
 
 
 int index_move(int cur_index, int steps_to_move){
